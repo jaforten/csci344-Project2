110dfcdfb3147d8dcfac2bfe81a2348f4536f859
Jack Fortenbery <jaforten@unca.edu>
2013-02-28 19:46:09 -0500

    touched up app.js with js lint, added white to twitter feed font color, added plugin with header, positioned the input, and should be finalized

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,31 +1,24 @@
-$(document).ready(function () {
-  $("#user_input").val("keyword"); 
-  $("#user_input_button").click(function () {
-   
-    $("#result_div p").html($("#user_input").val());
-    $("#interface_div").fadeOut(500, function () {
-      $("#result_div").css("font-family", "arial").fadeIn(500);
-      var input =  $("#user_input").val();
-      console.log("Twitter Feed");
-      var twitter = new ctwitter.CTwitter();
-      twitter.stream("statuses/filter", { lang:"en", track:[input] }, function (stream) {
-      stream.on("data", function (tweet) {
-        $('<p class="tweet"><img src="'+tweet.profile_image_url+'" />'+tweet.text+'</p>')
-		      .hide()
-		      .prependTo('#tweets')
-		      .slideDown(1000);
-          $('#tweets p:gt(0)')
-		      .fadeOut(700, function() { $(this).remove() });
-      });
+/*global $: false */
+var main = function () {
+    "use strict";
+    $("#user_input").val("keyword"); 
+    $("#user_input_button").click(function () {
+        var input =  $("#user_input").val();
+        $("#result_div p").html(input);
+        $("#interface_div").fadeOut(500, function () {
+            var twitter = new ctwitter.CTwitter();
+            $("#result_div").fadeIn(500);
+            console.log("Twitter Feed");
+            twitter.stream("statuses/filter", { lang: "en", track: [input] }, function (stream) {
+                stream.on("data", function (tweet) {
+                    $('<p class= "tweet"> <img src= "' + tweet.profile_image_url + '"/>' + tweet.text + '</p>')                                         
+                        .hide().prependTo('#tweets').slideDown(1000); 
+                    $('#tweets p:gt(0)').fadeOut(400, function () { 
+                        $(this).remove();
+                    });
+                });
+            });
+        });
     });
-    });
-  });
-  
-  
-
-
-
-
- 
- });
-
+}
+$(document).ready(main);
